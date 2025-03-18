88fd8b6951abcdcdff2675dc2f3bfc6c44b70d02
Hejsel <benjaminhejsel@gmail.com>
2025-03-18 08:34:03 +0100

    Vi kan nu registrerer admin til vores database og vi kan også logge ind!

--- restapi/models/dbhandlers.js
+++ restapi/models/dbhandlers.js
@@ -16,10 +16,29 @@ module.exports = {
 	insertUser: async function (req, res, next) {
 		try {
 			let db = await connect();
+			console.log("✅ Databaseforbindelse oprettet");
+
 			let sql = 'insert into user (email, password, bio) values(?, ?, ?)';
 			let query = db.prepare(sql);
+			console.log("📨 Data modtaget:", req.body.email, res.locals.hash, req.body.bio);
+
 			let row = await query.run(req.body.email, res.locals.hash, req.body.bio);
+			console.log("✅ Bruger gemt i databasen:", row);
+
 			res.locals.user = row
+		} catch (err) {
+			console.error("❌ Fejl i insertUser:", err.message);
+			res.status(400).json(err.message);
+		}
+	},
+
+	getUser: async function (req, res, next) {
+		try {
+			let db = await connect();
+			let sql = 'select * from user where email = ?';
+			let query = db.prepare(sql);
+			let row = await query.get(req.body.email);
+			res.locals.user = row;
 		} catch (err) {
 			res.status(400).json(err.message);
 		}
