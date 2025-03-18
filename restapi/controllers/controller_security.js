f8f36dd5f8f65b02ce80300842ae96eeddde45e8
madso99 <madsolesen1999@gmail.com>
2025-03-18 12:13:13 +0100

    console.log aktiv token

--- restapi/controllers/controller_security.js
+++ restapi/controllers/controller_security.js
@@ -1,25 +1,31 @@
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   // Middleware til at hashe passwords
-  hashPassword: async function(req, res, next) {
+  hashPassword: async function (req, res, next) {
     try {
-      res.locals.hash = await bcrypt.hash(req.body.password, parseInt(process.env.ROUNDS));
-      console.log('✅ hashPassword middleware blev udført!');
+      res.locals.hash = await bcrypt.hash(
+        req.body.password,
+        parseInt(process.env.ROUNDS)
+      );
+      console.log("✅ hashPassword middleware blev udført!");
       next();
     } catch (err) {
-      return res.status(500).json({ message: 'Error hashing password' });
+      return res.status(500).json({ message: "Error hashing password" });
     }
   },
 
   // Middleware til at validere password ved login
-  validatePassword: async function(req, res, next) {
+  validatePassword: async function (req, res, next) {
     try {
-      let match = await bcrypt.compare(req.body.password, res.locals.user.password);
-      if (!match) throw new Error('Invalid credentials');
-      console.log('✅ validatePassword middleware blev udført!');
+      let match = await bcrypt.compare(
+        req.body.password,
+        res.locals.user.password
+      );
+      if (!match) throw new Error("Invalid credentials");
+      console.log("✅ validatePassword middleware blev udført!");
       next();
     } catch (err) {
       return res.status(401).json({ message: err.message });
@@ -27,36 +33,43 @@ module.exports = {
   },
 
   // Middleware til at oprette et JWT-token
-  generateToken: async function(req, res, next) {
+  generateToken: async function (req, res, next) {
     try {
-      const payload = { email: res.locals.user.email, profile: res.locals.user.profile };
-      res.locals.token = await jwt.sign(payload, process.env.SECRET, { expiresIn: '1h' });
-      console.log('✅ generateToken middleware blev udført!');
+      const payload = {
+        email: res.locals.user.email,
+        profile: res.locals.user.profile,
+      };
+      res.locals.token = await jwt.sign(payload, process.env.SECRET, {
+        expiresIn: "1h",
+      });
+      console.log("✅ generateToken middleware blev udført!");
+      console.log("Genererede token", res.locals.token);
       //next();
     } catch (err) {
-      return res.status(500).json({ message: 'Error generating token' });
+      return res.status(500).json({ message: "Error generating token" });
     }
   },
 
   // Middleware til at validere JWT-token
-  validateToken: async function(req, res, next) {
+  validateToken: async function (req, res, next) {
     try {
-      let token = req.headers.authorization && req.headers.authorization.split(' ')[1];
-      if (!token) throw new Error('You must be logged in');
+      let token =
+        req.headers.authorization && req.headers.authorization.split(" ")[1];
+      if (!token) throw new Error("You must be logged in");
 
       let decoded = await jwt.verify(token, process.env.SECRET);
-      if (!decoded) throw new Error('Failed to authenticate token');
+      if (!decoded) throw new Error("Failed to authenticate token");
 
       if (Date.now() >= decoded.exp * 1000) {
-        throw new Error('Token has expired');
+        throw new Error("Token has expired");
       }
 
       res.locals.authorized = true;
       res.locals.profile = decoded.profile;
-      console.log('✅ validateToken middleware blev udført!');
+      console.log("✅ validateToken middleware blev udført!");
       next();
     } catch (err) {
       res.status(401).json({ message: err.message });
     }
-  }
+  },
 };
--- restapi/routes/admin.js
+++ restapi/routes/admin.js
@@ -1,36 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const ctrlAdmin = require('../controllers/controller_admin');
+const ctrlAdmin = require("../controllers/controller_admin");
 
 /* GET user register ie send form */
-router.get('/register', function(req, res, next) {
-  	res.render('register', {
-	  	title: 'Please Register',
-	  	subtitle: 'Follow the embedded cues'
-  	});
+router.get("/register", function (req, res, next) {
+  res.render("register", {
+    title: "Please Register",
+    subtitle: "Follow the embedded cues",
+  });
 });
 
-router.post('/register', ctrlAdmin.handleRegistration, function(req, res, next) {
-	res.status(201).json({message: 'Registration succesfull'});
+router.post(
+  "/register",
+  ctrlAdmin.handleRegistration,
+  function (req, res, next) {
+    res.status(201).json({ message: "Registration succesfull" });
+  }
+);
+
+router.post("/register", function (req, res, next) {
+  // variables from middleware
+  res.json({ login: "failed, please try again" });
 });
 
-router.post('/register', function(req, res, next) {
-	// variables from middleware
-	res.json({login: 'failed, please try again'});
+router.get("/login", function (req, res, next) {
+  res.render("login", {
+    title: "Please Login",
+  });
 });
 
-router.get('/login', function(req, res, next) {
-	res.render('login', {
-		title: 'Please Login'
-	});
-});
-
-router.post('/login', ctrlAdmin.handleLogin, function(req, res, next) {
-	/*res.render('admin_page', {
-		token: res.locals.token,
-		msg: 'Login successful'
-	});*/
+router.post("/login", ctrlAdmin.handleLogin, function (req, res, next) {
+  res.render("admin_page", {
+    token: res.locals.token,
+    msg: "Login successful",
+  });
 });
 
 module.exports = router;
